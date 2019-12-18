abc22c47f4e65466865b6662bbcc6776ae33903a
vpnlan715 <56340553+vpnlan715@users.noreply.github.com>
2019-12-18 15:58:54 +0300

    mmm

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("kiryuha1905@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: vpnlan715@gmail.com \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+?>
